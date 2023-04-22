3fed2eefcf56bcca1c9b2aeb507a65a6a0daf481
raydonbabs <78103414+raydonbabs@users.noreply.github.com>
2023-04-22 14:33:29 -0400

    Re-add model

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -1,33 +1,35 @@
-from App.models import User
+from werkzeug.security import check_password_hash, generate_password_hash
 from App.database import db
 
-def create_user(username, password):
-    newuser = User(username=username, password=password)
-    db.session.add(newuser)
-    db.session.commit()
-    return newuser
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username =  db.Column(db.String, nullable=False, unique=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    access = db.Column(db.String(120), nullable=False)
+    comps = db.relationship("Competition",backref="user", lazy=True, cascade = "all, delete-orphan")
 
-def get_user_by_username(username):
-    return User.query.filter_by(username=username).first()
+    def __init__(self, username, email,password,access):
+        self.username = username
+        self.email = email
+        self.set_password(password)
+        self.access = access
 
-def get_user(id):
-    return User.query.get(id)
+    def to_json(self):
+        return{
+            "id": self.id,
+            "username": self.username,
+            "email":self.email,
+            "access": self.access,
+        }
 
-def get_all_users():
-    return User.query.all()
-
-def get_all_users_json():
-    users = User.query.all()
-    if not users:
-        return []
-    users = [user.get_json() for user in users]
-    return users
-
-def update_user(id, username):
-    user = get_user(id)
-    if user:
-        user.username = username
-        db.session.add(user)
-        return db.session.commit()
-    return None
+    def set_password(self, password):
+        """Create hashed password."""
+        self.password = generate_password_hash(password, method='sha256')
     
+    def check_password(self, password):
+        """Check hashed password."""
+        return check_password_hash(self.password, password)
+    
+    def get_access(self):
+        return self.access
